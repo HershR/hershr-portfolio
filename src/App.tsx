e6f73c2114f43be18d6e76170641b647de0a47c2
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-22 17:09:27 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import React from "react";
+import { Header } from "./components/sections/Header";
+import { About } from "./components/sections/About";
+import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-gray-50 min-h-screen text-gray-800">
+      <Header />
+      <main className="px-4 md:px-12 lg:px-24 py-8 space-y-20">
+        <About />
+        <Projects username="HershR" />
+        <Contact />
+      </main>
+    </div>
+  );
 }
 
 export default App;
