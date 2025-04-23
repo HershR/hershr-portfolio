c97c6843c666940533fad2f49b0f8ee1cc5536b2
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2025-04-22 17:08:58 -0700

    Create About.tsx

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,18 @@
+export function About() {
+  return (
+    <section id="about" className="text-center">
+      <img
+        src="https://avatars.githubusercontent.com/u/113292983?v=4"
+        alt="HershR"
+        className="w-32 h-32 rounded-full mx-auto"
+      />
+      <h2 className="text-3xl font-semibold mt-4">Hersh R</h2>
+      <p className="text-gray-600 mt-2 max-w-xl mx-auto">
+        Junior Software Developer with experience in full-stack, mobile app, and
+        game development. Passionate about creating impactful solutions to
+        address real-world problems and contributing to innovative software
+        engineering projects
+      </p>
+    </section>
+  );
+}
